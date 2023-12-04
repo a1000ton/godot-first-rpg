c6d2d29ec4a3692a7bd28f5344ab39bbb899b168
Amilton Baracy <amiltonbjr@gmail.com>
2023-12-04 23:30:53 +0000

    Add bat chase func

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,20 +1,51 @@
 extends CharacterBody2D
 
 @export var bat_color:Color = "#fff"
-@export var knockback = Vector2.ZERO
+
+@export var ACCELERATION = 2000
+@export var MAX_SPEED = 75
+@export var KNOCKBACK_SPEED = 200
 
 @onready var animatedSprite = $AnimatedSprite
+@onready var playerDetectionZone = $PlayerDetectionZone
 @onready var stats = $Stats
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+var knockback = Vector2.ZERO
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = IDLE
 
 func _ready():
 	animatedSprite.modulate = bat_color
 
 func _physics_process(delta):
-	velocity = knockback.move_toward(Vector2.ZERO, 200 * delta)
-	knockback = velocity
+#	velocity = knockback.move_toward(Vector2.ZERO, KNOCKBACK_SPEED * delta)
+	
+	match state:
+		IDLE:
+#			velocity = velocity.move_toward(Vector2.ZERO, MAX_SPEED * delta)
+			seek_player()
+		WANDER:
+			pass
+			
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				
 	move_and_slide()
+			
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 	
 func create_death_effect():
 	var enemyDeathEffect = EnemyDeathEffect.instantiate()
